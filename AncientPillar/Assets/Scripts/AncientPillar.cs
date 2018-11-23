caf895bea3ab2a9c3d9b9795e892e476ba59a560
DeXoteric <marlev@poczta.pl>
2018-11-23 14:51:03 +0100

    initial Pillar setup, initial EnemySpawner setup

---
+++ AncientPillar/Assets/Scripts/AncientPillar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AncientPillar
+{
+    public class AncientPillar : MonoBehaviour
+    {
+
+        [SerializeField] private Transform spawnPoint;
+
+        // Offensive stats
+        private float damage;
+        private float attackSpeed;
+
+        // Defensive stats
+        private float health;
+        private float healthRegen;
+    }
+}
---
+++ AncientPillar/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AncientPillar
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+
+        [SerializeField] private Transform centralPoint;
+        [SerializeField] private float initialSpawnDelay;
+
+        [Header("Enemies")]
+        [SerializeField] private GameObject enemyPrefab;
+
+        private int phase = 1; // +1 every 10 waves
+        private int wave = 1;
+
+        
+
+
+
+        private void Start()
+        {
+            StartCoroutine(SpawnEnemies());
+        }
+
+
+        IEnumerator  SpawnEnemies()
+        {
+            
+            
+            ActivateNextPhase();
+            wave += 1;
+            Debug.Log(wave + " - " + phase);
+            yield return new WaitForSeconds(initialSpawnDelay);
+            StartCoroutine(SpawnEnemies());
+        }
+
+        
+
+        private void ActivateNextPhase()
+        {
+            
+            if (wave % 10 == 0)
+            {
+                phase += 1;
+            }
+        }
+    }
+}
